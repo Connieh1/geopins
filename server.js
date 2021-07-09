9b6267e75521e19c1ee1c3f09ee1e7857d6ff7ef
Connie Hyman <connie2630a@gmail.com>
2021-07-08 21:38:57 -0700

    Add user authentication from Apollo Server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const typeDefs = require("./typeDefs");
 
 const resolvers = require("./resolvers");
-// const { isCompositeType } = require("graphql");
-
-const mongoose = require("mongoose");
-
-require("dotenv").config();
+const { findOrCreateUser } = require("./controllers/userController");
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -20,6 +19,20 @@ mongoose
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        //find or create user
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
+    }
+    return { currentUser };
+  },
 });
 
 server.listen().then(({ url }) => {
