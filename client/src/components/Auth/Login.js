572e29d1ab808876d152fd3c0f8e7fa5a1f3024f
Connie Hyman <connie2630a@gmail.com>
2021-07-08 19:02:32 -0700

    Set up Google OAuth and add Google Login Button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = (googleUser) => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({ idToken });
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId="49826324425-806c996359vh8am9h3lf8qp91rrmttb7.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        isSignedIn={true}
+      />
+    </div>
+  );
 };
 
 const styles = {
@@ -12,8 +26,8 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export default withStyles(styles)(Login);
